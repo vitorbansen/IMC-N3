ec42676e1c272852b519e5d4a57852896a6488de
vitorbansen <105083943+vitorbansen@users.noreply.github.com>
2023-12-14 19:33:08 -0300

    projeto imc

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\vitor\\OneDrive\\Área de Trabalho\\n3Flutter\\n3flutter" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\vitor\\OneDrive\\Área de Trabalho\\n3Flutter\\n3flutter"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\vitor\\OneDrive\\Área de Trabalho\\n3Flutter\\n3flutter\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\vitor\\OneDrive\\Área de Trabalho\\n3Flutter\\n3flutter"
+  "FLUTTER_TARGET=C:\\Users\\vitor\\OneDrive\\Área de Trabalho\\n3Flutter\\n3flutter\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8xYWM2MTFjNjRlYWRiZDkzYzVmNWFiYTU0OTRiOGZjM2IzNWVlOTUyLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\vitor\\OneDrive\\Área de Trabalho\\n3Flutter\\n3flutter\\.dart_tool\\package_config.json"
+)
